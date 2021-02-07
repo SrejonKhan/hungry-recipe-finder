59a911372dd1d68ebe812c1322c6f5bc8d68d0c6
Srejon Khan <srejonkhan79@gmail.com>
2021-02-07 13:51:32 +0600

    core functionalities

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
-let x;
-fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=chicken').
+const searchInput = document.getElementById("search-inputfield");
+const searchButton = document.getElementById("search-button");
+
+const recipeDiv = document.getElementById("individual-recipe");
+
+
+searchButton.addEventListener("click", () => fetchRecipes(searchInput.value));
+
+function fetchRecipes(keyword) { 
+fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${keyword}`).
     then(response => response.json()).
     then(json =>
     {
         const resultGroupDiv = document.getElementById("result-groups");
+        resultGroupDiv.innerHTML = "";
+     
+        const notFoundDiv = document.getElementById("not-found-div");
+        notFoundDiv.style.display = "none";
+
+        recipeDiv.style.display = "none";
+        
+        if (json.meals == null || json.meals.length == 0) { 
+            notFoundDiv.style.display = "block";
+            return;
+        }
         
         for (let i = 0; i < json.meals.length; i++) { 
             const meal = json.meals[i];
@@ -11,18 +30,18 @@ fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=chicken').
             card.className = "col";
             const cardInnerHTML = `<div class="card recipe-card">
             <img src="${meal.strMealThumb}" class="card-img-top" alt="..." />
-            <div class="card-body">
-              <h5 class="card-title">${meal.strMeal}</h5>
+            <div class="card-body text-center">
+              <h5 class="card-title recipe-card-title">${meal.strMeal}</h5>
             </div>`;
             card.innerHTML = cardInnerHTML;
             resultGroupDiv.appendChild(card);
-            card.addEventListener("click", () => showRecipe(meal));
+            card.addEventListener("click", () => showIndividualRecipe(meal));
         }
     });
+}
 
-function showRecipe(recipe) { 
+function showIndividualRecipe(recipe) { 
     console.log(recipe);
-    const recipeDiv = document.getElementById("individual-recipe");
     recipeDiv.style.display = "block";
 
     const recipeThumbnail = document.getElementById("individual-recipe-image");
@@ -32,13 +51,16 @@ function showRecipe(recipe) {
     recipeTitle.innerText = recipe.strMeal;
 
     const recipeIngredientsList = document.getElementById("individual-recipe-ingredients");
-
+    recipeIngredientsList.innerHTML = "";
     for (let i = 0; i < 20; i++) { 
         if (Object.prototype.hasOwnProperty.call(recipe, "strIngredient" + (i + 1))) { 
+            if (recipe['strIngredient' + (i + 1)].length <= 0)
+                continue;
             const ingredientItem = document.createElement("li");
             ingredientItem.innerText = recipe['strIngredient' + (i + 1)];         
             recipeIngredientsList.appendChild(ingredientItem);
         }
-
     }
+      document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+
 }    
